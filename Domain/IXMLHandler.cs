ec5744f45bde207814912f081d6c4a24e097331e
Tomas Petrlik <tomas.petrlik@pocketvirtuality.com>
2025-10-02 22:20:12 +0200

    Generic XML handler

---
+++ Domain/IXMLHandler.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain;
+
+public interface IXMLHandler
+{
+    public T? Load<T>(string filePath) where T : class;
+    public void Save<T>(T obj, string filePath) where T : class;
+}
---
+++ Infrastructure/Configuration/GenericXMLHandler.cs
@@ -0,0 +1,29 @@
+﻿using Domain;
+using System.Xml.Serialization;
+
+namespace Infrastructure.Configuration;
+public class GenericXMLHandler : IXMLHandler
+{
+    public T? Load<T>(string filePath) where T : class
+    {
+        if (!File.Exists(filePath))
+        {
+            throw new FileNotFoundException($"The file at path {filePath} was not found.");
+        }
+
+        XmlSerializer serializer = new(typeof(T));
+        using (var reader = new StreamReader(filePath))
+        {
+            return serializer.Deserialize(reader) as T;
+        }
+    }
+
+    public void Save<T>(T obj, string filePath) where T : class
+    {
+        XmlSerializer serializer = new(typeof(T));
+        using (var writer = new StreamWriter(filePath, false))
+        {
+            serializer.Serialize(writer, obj);
+        }
+    }
+}
