c6da10d89b212bee156da248763b1b84b469aa61
Tomas Petrlik <tomas.petrlik@pocketvirtuality.com>
2025-10-02 22:20:26 +0200

    Configuration

---
+++ Infrastructure/Configuration/Configuration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Configuration;
+
+[Serializable]
+public class Configuration
+{
+    public bool IsOutputToFolderEnabled = true;
+    public string OutputPath = string.Empty;
+    public string LastEvaluatedFile = "None";
+}
---
+++ Infrastructure/Configuration/ConfigurationManager.cs
@@ -0,0 +1,65 @@
+﻿using Domain;
+using System.Xml.Serialization;
+
+namespace Infrastructure.Configuration;
+public class ConfigurationManager
+{
+    public const string ConfigurationFileName = "IntegerCalculatorSettings.xml";
+    public Configuration Configuration { get; private set; }
+    IXMLHandler xmlHandler;
+
+    public ConfigurationManager(IXMLHandler xmlHandler)
+    {
+        this.xmlHandler = xmlHandler;
+        Load();
+    }
+
+    public void ChangeLastEvaluatedFile(string evaluatedFilePath)
+    {
+        if (Configuration.LastEvaluatedFile == evaluatedFilePath)
+            return;
+
+        Configuration.LastEvaluatedFile = evaluatedFilePath;
+        Save();
+    }
+
+    public void ChangeOutputFolderPath(string outputFolderPath)
+    {
+        if (Configuration.OutputPath == outputFolderPath)
+            return;
+
+        Configuration.OutputPath = outputFolderPath;
+        Save();
+    }
+
+    public void ChangeOutputExportOptionPath(bool isEnabled)
+    {
+        if(Configuration.IsOutputToFolderEnabled == isEnabled)
+            return;
+
+        Configuration.IsOutputToFolderEnabled = isEnabled;
+        Save();
+    }
+
+    private void Save()
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(Configuration));
+        var path = Path.Combine(Directory.GetCurrentDirectory(), ConfigurationFileName);
+
+        xmlHandler.Save(Configuration, path);
+    }
+
+    private void Load()
+    {
+        var path = Path.Combine(Directory.GetCurrentDirectory(), ConfigurationFileName);
+
+        try
+        {
+            Configuration = xmlHandler.Load<Configuration>(path)!;   // This throws FileNotFoundException on every first run but I do not want to bother user with it. Could be logged somewhere that Config was initialized.
+        }
+        catch
+        {
+            Configuration = new ();
+        }
+    }
+}
